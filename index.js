9365d8af752c4205b0538b9a0c583a9f686d7ef7
LittleJohn22 <132698329+LittleJohn22@users.noreply.github.com>
2023-08-11 13:09:02 +0000

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //index.js
 import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
-import { selectUsuarios } from "./bd.js";
+import { selectUsuarios, selectUsuario } from "./bd.js";
 
 dotenv.config();     
 
@@ -27,4 +27,16 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+//index.js
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
