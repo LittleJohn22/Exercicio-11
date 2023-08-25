108d357dc5cfe64f2f04309a43a6297c3cd5019b
LittleJohn22 <132698329+LittleJohn22@users.noreply.github.com>
2023-08-25 17:06:46 +0000

    .

--- src/index.js
+++ src/index.js
@@ -1,38 +1,28 @@
 //index.js
 import dotenv from "dotenv";
-import express from "express";      // Requisição do pacote do express
-//index.js
-import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./bd.js";
+dotenv.config();
 
-dotenv.config();     
+import express from "express";   
 
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 import roteadorUsuario from "./routes/usuario.js";
+import loginRouter from "./routes/login.js";
 
 app.use(roteadorUsuario);
-
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-  console.log(`Serviço escutando na porta:  ${port}`);
-});
-
-app.get("/usuarios", async (req, res) => {
-  console.log("Rota GET/usuarios solicitada");
-  try {
-    const usuarios = await selectUsuarios();
-    res.json(usuarios);
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
+app.use(loginRouter);
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
-    nome: "John Lucas Ferreira Rodrigues",      // Substitua pelo seu nome
+    nome: "Gabriela de Souza Reis",      // Substitua pelo seu nome
   });
   console.log("Rota / solicitada");
 });
 
-
-
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
